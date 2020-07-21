16a91736b7faaa2759bebbc74090286680cccf59
Alabi Emmanuel <bobslegend795@gmail.com>
2020-07-21 04:59:43 +0100

    Updates

---
+++ precache-manifest.4951992a6f3d3bb7ed3b4e8298e12525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b412a0341d4419225333b90ceecd4c",
+    "url": "/paystack-test-cookies/index.html"
+  },
+  {
+    "revision": "6f2c9dee26b2bf80c0e4",
+    "url": "/paystack-test-cookies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf7be9aea3ed6d7802f6",
+    "url": "/paystack-test-cookies/static/js/2.000bc207.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/paystack-test-cookies/static/js/2.000bc207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2c9dee26b2bf80c0e4",
+    "url": "/paystack-test-cookies/static/js/main.16fe7e67.chunk.js"
+  },
+  {
+    "revision": "2cfd264cde0861d158e2",
+    "url": "/paystack-test-cookies/static/js/runtime-main.99374012.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/paystack-test-cookies/precache-manifest.4951992a6f3d3bb7ed3b4e8298e12525.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paystack-test-cookies/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
